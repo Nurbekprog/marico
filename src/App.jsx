1d6c090d56aa7f963577e8d1f13e3a353cfa62de
Nurbekprog <nurprogdata@gmail.com>
2024-03-09 19:02:22 +0500

    step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Stepone from "./components/step1/Stepone";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Home />
+      <Stepone />
     </div>
   );
 };
---
+++ src/components/step1/Stepone.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Stepone.scss";
+
+const Stepone = () => {
+  return (
+    <div className="step1 container">
+      <div className="step1-head">
+        <h6>STEP 1</h6>
+        <h2>Connect Your Content</h2>
+        <p>
+          Bring all of your content together and get a Homepage that
+          automatically updates whenever you create anywhere online.
+        </p>
+        <button>View Avaliable Sources</button>
+      </div>
+      <div className="step1-body">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Stepone;
