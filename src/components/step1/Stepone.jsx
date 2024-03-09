1b71f9063845e2f252347afb7e48ede73f61b648
Nurbekprog <nurprogdata@gmail.com>
2024-03-09 22:37:48 +0500

    step1-finished

--- src/components/step1/Stepone.jsx
+++ src/components/step1/Stepone.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Stepone.scss";
-
+import stepOne from "../../assets/images/step-one-img.png";
 const Stepone = () => {
   return (
     <div className="step1 container">
@@ -14,7 +14,30 @@ const Stepone = () => {
         <button>View Avaliable Sources</button>
       </div>
       <div className="step1-body">
-        
+        <div className="step1-body-text">
+          <h5>Your Homepage</h5>
+          <div>
+            <h2 className="h21">Your Content.</h2>
+            <h2>
+              All in <p className="h2">One Spot</p>
+            </h2>
+            <ul>
+              <li>
+                <p className="p1">1</p> Bring all of your content together into
+                one homepage.
+              </li>
+              <li>
+                <p className="p2">2</p> Your page automatically updates whenever
+                you create.
+              </li>
+            </ul>
+            <div className="step1-body-text-btns">
+              <button className="step1-body-text-btns-1">Get Started Now</button>
+              <button className="step1-body-text-btns-2">View A Demo</button>
+            </div>
+          </div>
+        </div>
+        <img src={stepOne} alt="" />
       </div>
     </div>
   );
