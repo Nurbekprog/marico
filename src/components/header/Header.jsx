de0fab9126b7f898def59e6939acda430dc5f713
Nurbekprog <nurprogdata@gmail.com>
2024-03-10 20:36:34 +0500

    project-finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import logo from "../../assets/images/logo.svg";
 import menuBtn from "../../assets/images/menu-btn.svg";
+import close from "../../assets/images/close.svg";
 const Header = () => {
+  const [isToggle, setToggle] = useState(false);
+  const toggle = () => {
+    setToggle(!isToggle);
+    console.log(`${isToggle} `);
+  };
   return (
     <div className="">
       <div className="container nav">
@@ -25,7 +31,34 @@ const Header = () => {
         <div className="nav-btns">
           <button className="nav-btns-1">Login</button>
           <button className="nav-btns-2">Sign Up</button>
-          <img src={menuBtn} alt="" />
+          <button className="nav-btns-3" onClick={toggle}>
+            {isToggle ? <img src={close} /> : <img src={menuBtn} />}
+            {isToggle ? (
+              <div className="menubar">
+                <select className="dropdown" name="" id="">
+                  <option value="">Use Cases</option>
+                  <option value="example">Example</option>
+                  <option value="example">Example</option>
+                </select>
+                <a href="">About</a>
+                <a href="">Pricing</a>
+                <a href="">Blog</a>
+                <button className="">Login</button>
+                <button className="">Sign Up</button>
+              </div>
+            ) : (
+              <div className="menu-none">
+                <select className="dropdown" name="" id="">
+                  <option value="">Use Cases</option>
+                  <option value="example">Example</option>
+                  <option value="example">Example</option>
+                </select>
+                <a href="">About</a>
+                <a href="">Pricing</a>
+                <a href="">Blog</a>
+              </div>
+            )}
+          </button>
         </div>
       </div>
     </div>
