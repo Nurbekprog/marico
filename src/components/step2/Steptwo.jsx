82fcef20871c71d0768ae010a177786619e6ac04
Nurbekprog <nurprogdata@gmail.com>
2024-03-10 15:40:09 +0500

    step2-finished

--- src/components/step2/Steptwo.jsx
+++ src/components/step2/Steptwo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Steptwo.scss";
+import phone from "../../assets/images/phone.png";
 const Steptwo = () => {
   return (
     <div className="step2 container">
@@ -12,8 +13,21 @@ const Steptwo = () => {
         </p>
       </div>
       <div className="step2-body">
-        
+        <div>
+          <p>One Link</p>
+          <h2>ALL You Create.</h2>
+          <h3>One Link</h3>
+          <img src={phone} alt="" />
+        </div>
+        <div>
+          <p>Collect Subscribers</p>
+          <h2>
+            Emails. <br /> Phone #s.
+          </h2>
+          <h3>All Yours.</h3>
+        </div>
       </div>
+      <button>Get Started </button>
     </div>
   );
 };
